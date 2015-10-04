306ece02178c8357f050921dff76dd54c1f2b403
akeenan2 <ann.keenan2@gmail.com>
2015-10-03 23:58:52 -0400

    list select

--- app/src/main/java/com/foodvault/foodvault/RecipeActivity.java
+++ app/src/main/java/com/foodvault/foodvault/RecipeActivity.java
@@ -1,5 +1,6 @@
 package com.foodvault.foodvault;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.RatingBar;
@@ -14,14 +15,17 @@ public class RecipeActivity extends AppCompatActivity {
         DatabaseHandler db = new DatabaseHandler(this);
         db.addRecipe(new Recipe(1, "Enchiladas", "https://allrecipes.com", "40", "flour", "4",
                 "Great Recipe"));
-        Recipe thisRecipe = db.getRecipe(1);
+        Intent myIntent = getIntent(); // gets the previously created intent
+        String idString = myIntent.getStringExtra("ID");
+        int ID = Integer.parseInt(idString);
+        Recipe thisRecipe = db.getRecipe(ID);
 
         TextView recipeName = (TextView) findViewById(R.id.textView1);
         recipeName.setText(thisRecipe.getName());
         TextView recipeUrl = (TextView) findViewById(R.id.textView2);
         recipeUrl.setText(thisRecipe.getUrl());
         TextView recipePrepTime = (TextView) findViewById(R.id.textView3);
-        recipePrepTime.setText(thisRecipe.getPrepTime());
+        recipePrepTime.setText(thisRecipe.getPrepTime()+" min");
         TextView recipeIngredients = (TextView) findViewById(R.id.textView4);
         recipeIngredients.setText(thisRecipe.getIngredients());
         TextView recipeRating = (TextView) findViewById(R.id.textView5);
